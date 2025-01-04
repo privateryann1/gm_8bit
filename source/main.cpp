b710fdb3bdb87a4cbedddcfc74e6cb4b48832e45
32east <tvrar30@gmail.com>
2025-01-04 23:43:46 +0600

    вернуть как было

--- source/main.cpp
+++ source/main.cpp
@@ -126,7 +126,7 @@ void hook_BroadcastVoiceData(IClient* cl, uint nBytes, char* data, int64 xuid) {
 				
 				        if (i < (sizeof(decompressedBuffer) / sizeof(uint16_t))) {
 				            int16_t signedSample = static_cast<int16_t>(LAU->GetNumber(-1));
-				            reinterpret_cast<int16_t*>(decompressedBuffer)[i] = signedSample * 2.0f;
+				            reinterpret_cast<int16_t*>(decompressedBuffer)[i] = signedSample;
 				        }
 				
 				        LAU->Pop();
