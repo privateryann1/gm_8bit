f25ed484b0dca193bd9cea07ddf0d9668046ff59
32east <tvrar30@gmail.com>
2025-01-05 22:26:26 +0600

    Update eightbit_state.h

--- source/eightbit_state.h
+++ source/eightbit_state.h
@@ -4,8 +4,7 @@
 
 struct EightbitState {
 	bool broadcastPackets = false;
-	bool sample_rate = 24000;
-
+	uint16_t sample_rate = 24000;
 	uint16_t port = 4000;
 	std::string ip = "127.0.0.1";
 	std::unordered_map<int, std::tuple<IVoiceCodec*, int>> afflictedPlayers;
