083225d57e3b5b995e2fdfd2f899b15b8c27fcb4
32east <tvrar30@gmail.com>
2025-01-05 22:24:39 +0600

    sample_rate

--- source/eightbit_state.h
+++ source/eightbit_state.h
@@ -3,10 +3,9 @@
 #include <unordered_map>
 
 struct EightbitState {
-	int crushFactor = 350;
-	float gainFactor = 1.2;
 	bool broadcastPackets = false;
-	int desampleRate = 2;
+	bool sample_rate = 24000;
+
 	uint16_t port = 4000;
 	std::string ip = "127.0.0.1";
 	std::unordered_map<int, std::tuple<IVoiceCodec*, int>> afflictedPlayers;
